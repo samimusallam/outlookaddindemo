a02fb4672f9e7199f8356a35ffbb128269d8b3c8
Sami Musallam <sami.musallam7@gmail.com>
2020-11-06 13:51:31 +0200

    Update MessageRead.js

--- OutlookWebAddIn1Web/MessageRead.js
+++ OutlookWebAddIn1Web/MessageRead.js
@@ -11,17 +11,34 @@
     });
 
     function loadItemProps(item) {
-       var body = item.body;
-        $('#item-MessageBody').text("Initial Body");
-        body.getAsync(Office.CoercionType.Html, function (asyncResult) {
-            if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
-                console.log(asyncResult);
-                $('#item-MessageBody').text("Failed to get body");
+//        var body = item.body;
+//         $('#item-MessageBody').text("Initial Body");
+//         body.getAsync(Office.CoercionType.Html, function (asyncResult) {
+//             if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+//                 console.log(asyncResult);
+//                 $('#item-MessageBody').text("Failed to get body");
+//             }
+//             else {
+//                 $('#item-MessageBody').text("JSON: " + JSON.stringify(asyncResult, null, 2));
+//             }
+//         });    
+        
+        var outputString = "";
+
+        if (item.attachments.length > 0) {
+            for (i = 0 ; i < item.attachments.length ; i++) {
+                var attachment = item.attachments[i];
+                outputString += "<BR>" + i + ". Name: ";
+                outputString += attachment.name;
+                outputString += "<BR>ID: " + attachment.id;
+                outputString += "<BR>contentType: " + attachment.contentType;
+                outputString += "<BR>size: " + attachment.size;
+                outputString += "<BR>attachmentType: " + attachment.attachmentType;
+                outputString += "<BR>isInline: " + attachment.isInline;
             }
-            else {
-                $('#item-MessageBody').text("JSON: " + JSON.stringify(asyncResult, null, 2));
-            }
-        });       
+        }
+        
+        $('#item-MessageBody').text(outputString);
         
         // Write message property values to the task pane
         $('#item-id').text(item.itemId);
