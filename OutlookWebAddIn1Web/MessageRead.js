1a86c74fae253fadfb76a015f501501ff32fb0dd
Sami Musallam <sami.musallam7@gmail.com>
2020-11-06 13:11:46 +0200

    Update MessageRead.js

--- OutlookWebAddIn1Web/MessageRead.js
+++ OutlookWebAddIn1Web/MessageRead.js
@@ -11,10 +11,19 @@
     });
 
     function loadItemProps(item) {
+        var body = item.body;
+        body.getAsync(Office.CoercionType.Text, function (asyncResult) {
+            if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+            }
+            else {
+                $('#item-internetMessageId').text(asyncResult.value.trim());
+            }
+        });         
+        
         // Write message property values to the task pane
         $('#item-id').text(item.itemId);
         $('#item-subject').text(item.subject);
-        $('#item-internetMessageId').text(item.body);
+        $('#item-internetMessageId').text();
         $('#item-from').html(item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;");
     }
 })();
