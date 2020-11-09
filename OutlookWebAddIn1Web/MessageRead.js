d9dc338901f587d18010dfaed03875cd0272c6dc
Sami Musallam <sami.musallam7@gmail.com>
2020-11-09 13:09:00 +0200

    Update MessageRead.js

--- OutlookWebAddIn1Web/MessageRead.js
+++ OutlookWebAddIn1Web/MessageRead.js
@@ -2,57 +2,17 @@
 
 (function () {
 
+    var xhr;
+var serviceRequest;
+    
     Office.onReady(function () {
         // Office is ready
         $(document).ready(function () {
             // The document is ready
-            loadItemProps(Office.context.mailbox.item);
-            setItemBody(Office.context.mailbox.item);
+            initApp();
+//             loadItemProps(Office.context.mailbox.item);
         });
     });
-
-    function setItemBody(item) {
-                    item.body.setSelectedDataAsync(
-                        '<b> Kindly note we now open 7 days a week.</b>',
-                        { coercionType: Office.CoercionType.Html, 
-                        asyncContext: { var3: 1, var4: 2 } },
-                        function (asyncResult) {
-                            if (asyncResult.status == 
-                                Office.AsyncResultStatus.Failed){
-                                write(asyncResult.error.message);
-                            }
-                            else {
-                                // Successfully set data in item body.
-                                // Do whatever appropriate for your scenario,
-                                // using the arguments var3 and var4 as applicable.
-                            }
-                        });
-//                 }
-//                 else {
-//                     // Body is of text type. 
-//                     item.body.setSelectedDataAsync(
-//                         ' Kindly note we now open 7 days a week.',
-//                         { coercionType: Office.CoercionType.Text, 
-//                             asyncContext: { var3: 1, var4: 2 } },
-//                         function (asyncResult) {
-//                             if (asyncResult.status == 
-//                                 Office.AsyncResultStatus.Failed){
-//                                 write(asyncResult.error.message);
-//                             }
-//                             else {
-//                                 // Successfully set data in item body.
-//                                 // Do whatever appropriate for your scenario,
-//                                 // using the arguments var3 and var4 as applicable.
-//                             }
-//                          });
-//                 }
-//             }
-//         });
-}
-    
-   function write(message){
-    document.getElementById('message').innerText += message; 
-   }
     
    function loadItemProps(item) {
 //        var body = item.body;
@@ -89,4 +49,116 @@
         $('#item-subject').text(item.subject);
         $('#item-from').html(item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;");
     }
+    
+        function initApp() {
+        $("#footer").hide();
+
+        if (Office.context.mailbox.item.attachments == undefined) {
+            var testButton = document.getElementById("testButton");
+            testButton.onclick = "";
+            showToast("Not supported", "Attachments are not supported by your Exchange server.");
+        } else if (Office.context.mailbox.item.attachments.length == 0) {
+            var testButton = document.getElementById("testButton");
+            testButton.onclick = "";
+            showToast("No attachments", "There are no attachments on this item.");
+        } else {
+
+            // Initalize a context object for the app.
+            //   Set the fields that are used on the request
+            //   object to default values.
+            serviceRequest = new Object();
+            serviceRequest.attachmentToken = "";
+            serviceRequest.ewsUrl = Office.context.mailbox.ewsUrl;
+            serviceRequest.attachments = new Array();
+        }
+    };
+
+})();
+
+function testAttachments() {
+    Office.context.mailbox.getCallbackTokenAsync(attachmentTokenCallback);
+};
+
+function attachmentTokenCallback(asyncResult, userContext) {
+    if (asyncResult.status == "succeeded") {
+        serviceRequest.attachmentToken = asyncResult.value;
+        makeServiceRequest();
+    }
+    else {
+        showToast("Error", "Could not get callback token: " + asyncResult.error.message);
+    }
+}
+
+function makeServiceRequest() {
+    var attachment;
+    xhr = new XMLHttpRequest();
+
+    // Update the URL to point to your service location.
+    xhr.open("POST", "https://localhost:44320/api/AttachmentService", true);
+
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.onreadystatechange = requestReadyStateChange;
+
+    // Translate the attachment details into a form easily understood by WCF.
+    for (var i = 0; i < Office.context.mailbox.item.attachments.length; i++) {
+        attachment = Office.context.mailbox.item.attachments[i];
+        attachment = attachment._data$p$0 || attachment.$0_0;
+
+        if (attachment !== undefined) {
+            serviceRequest.attachments[i] = JSON.parse(JSON.stringify(attachment));
+        }
+    }
+
+    // Send the request. The response is handled in the 
+    // requestReadyStateChange function.
+    xhr.send(JSON.stringify(serviceRequest));
+};
+
+
+// Handles the response from the JSON web service.
+function requestReadyStateChange() {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            if (!response.isError) {
+                // The response indicates that the server recognized
+                // the client identity and processed the request.
+                // Show the response.
+                var names = "<h2>Attachments processed: " + response.attachmentsProcessed + "</h2>";
+
+                for (var i = 0; i < response.attachmentNames.length; i++) {
+                    names += response.attachmentNames[i] + "<br />";
+                }
+                document.getElementById("names").innerHTML = names;
+            } else {
+                showToast("Runtime error", response.message);
+            }
+        } else {
+            if (xhr.status == 404) {
+                showToast("Service not found", "The app server could not be found.");
+            } else {
+                showToast("Unknown error", "There was an unexpected error: " + xhr.status + " -- " + xhr.statusText);
+            }
+        }
+    }
+};
+
+// Shows the service response.
+function showResponse(response) {
+    showToast("Service Response", "Attachments processed: " + response.attachmentsProcessed);
+}
+
+// Displays a message for 10 seconds.
+function showToast(title, message) {
+
+    var notice = document.getElementById("notice");
+    var output = document.getElementById('output');
+
+    notice.innerHTML = title;
+    output.innerHTML = message;
+
+    $("#footer").show("slow");
+
+    window.setTimeout(() => { $("#footer").hide("slow") }, 10000);
+};
 })();
